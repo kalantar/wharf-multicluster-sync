30602f9a23c6b180370acbdb512ffa0b532d492b
Michael Kalantar <kalantar@us.ibm.com>
2018-10-29 10:09:56 -0400

    make install_citadel more flexible

--- docs/install/delete_citadel.sh
+++ docs/install/delete_citadel.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
+set -e -x
 
+
+DELETE_ROOTCA=
 NAMESPACE=istio-system
+declare -a CLUSTERS
+while (( $# > 0 )); do
+  case "${1}" in
+    --root-ca|--rootca)
+       ROOTCA="${2}"
+       shift; shift
+       ;;
+    --delete-root-ca|--delete-rootca)
+       DELETE_ROOTCA=true
+       shift
+       ;;
+    --install-namespace)
+       NAMESPACE="${2}"
+       shift; shift
+       ;;
+    *) CLUSTERS+=( "${1}" )
+       shift
+       ;;
+  esac
+done
 
-rootca_host=`kubectl --context ${ROOTCA} get service standalone-citadel -n istio-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
+
+rootca_host=`kubectl --context ${ROOTCA} get service standalone-citadel -n ${NAMESPACE} -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
 # handle case of no LB (ex ICP)
 if [[ -z $rootca_host ]]; then
   rootca_host=`kubectl --context ${ROOTCA} get nodes | grep proxy | awk '{print $1}'`
-  rootca_port=`kubectl --context ${ROOTCA} get service standalone-citadel -n istio-system -o jsonpath='{.spec.ports[0].nodePort}'`
+  rootca_port=`kubectl --context ${ROOTCA} get service standalone-citadel -n ${NAMESPACE} -o jsonpath='{.spec.ports[0].nodePort}'`
 fi
 
-for CLUSTER in ${CLUSTER1} ${CLUSTER2}
-do
-
+for CLUSTER in "${CLUSTERS[@]}"; do
   SERVICE_ACCOUNT="istio-citadel-service-account-${CLUSTER}"
   CERT_NAME="istio.${SERVICE_ACCOUNT}"
 
@@ -22,7 +44,7 @@ do
     sed -e "s/__ROOTCA_HOST__/${rootca_host}/g" istio-citadel-new-svc-lb.yaml \
       | kubectl --context ${CLUSTER} delete -f --ignore-not-found
   fi
-  sed -e "s/__CLUSTERNAME__/${CLUSTER}/g" istio-citadel-new.yaml \
+  sed -e "s/__CLUSTERNAME__/${CLUSTER}/g" istio-citadel.yaml \
     | kubectl --context ${CLUSTER} delete -f - --ignore-not-found
 
   kubectl --context ${CLUSTER} -n ${NAMESPACE} delete secret cacerts --ignore-not-found
@@ -30,8 +52,11 @@ do
   kubectl --context ${ROOTCA} -n ${NAMESPACE} delete serviceaccount ${SERVICE_ACCOUNT} --ignore-not-found
   kubectl --context ${ROOTCA} -n ${NAMESPACE} delete secret ${CERT_NAME} --ignore-not-found
 
-  sed -e "s/__SERVICE_ACCOUNT__/${SERVICE_ACCOUNT}/g" istio-citadel_on_delete.yaml | kubectl --context ${CLUSTER} apply -f -
+  kubectl --context ${CLUSTER} apply -f istio-citadel-ondelete.yaml
 done
 
-kubectl --context ${ROOTCA} delete -f istio-citadel-standalone.yaml --ignore-not-found
+if [[ -n "${DELETE_ROOTCA}" ]]; then
+  kubectl --context ${ROOTCA} delete -f istio-citadel-standalone.yaml --ignore-not-found
+fi
+
 
--- docs/install/install_citadel.sh
+++ docs/install/install_citadel.sh
@@ -1,47 +1,115 @@
 #!/bin/bash
 set -e -x
 
-# Install the ROOT CA
-kubectl --context ${ROOTCA} apply -f istio-citadel-standalone.yaml
-rootca_host=`kubectl --context ${ROOTCA} get service standalone-citadel -n istio-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
-# handle case of no LB (ex ICP)
-if [[ -z $rootca_host ]]; then
-  rootca_host=`kubectl --context ${ROOTCA} get nodes | grep proxy | awk '{print $1}'`
-  rootca_port=`kubectl --context ${ROOTCA} get service standalone-citadel -n istio-system -o jsonpath='{.spec.ports[0].nodePort}'`
-fi
+function usage() {
+  echo "Usage $0 --root-ca root_ca_context [--install-root-ca] k8s_context*"
+  exit 1
+}
 
-NAMESPACE="istio-system"
+INSTALL_ROOTCA=
+NAMESPACE=istio-system
+declare -a CLUSTERS
+while (( $# > 0 )); do
+  case "${1}" in
+    --root-ca|--rootca)
+       ROOTCA="${2}"
+       shift; shift
+       ;;
+    --install-root-ca|--install-rootca)
+       INSTALL_ROOTCA=true
+       shift
+       ;;
+    --install-namespace)
+       NAMESPACE="${2}"
+       shift; shift
+       ;;
+    *) CLUSTERS+=( "${1}" )
+       shift
+       ;;
+  esac
+done
+
 B64_DECODE=${BASE64_DECODE:-base64 --decode}
 
-for CLUSTER in ${CLUSTER1} ${CLUSTER2} #MK#${CLUSTER3}
-do
-  SERVICE_ACCOUNT=$(echo "istio-citadel-service-account-$CLUSTER" | tr '[:upper:]' '[:lower:]') #lower case to make it valid service account name
-  kubectl --context ${ROOTCA} -n istio-system create serviceaccount ${SERVICE_ACCOUNT} || true
-  CERT_NAME="istio.${SERVICE_ACCOUNT}"
-  DIR="/tmp/ca/${CLUSTER}"
-  mkdir -p $DIR
+function install_root_ca() {
+  echo "Installing root CA on cluster ${ROOTCA}"
+  kubectl --context ${ROOTCA} apply -f istio-citadel-standalone.yaml
+}
 
+function configure_client_cluster() {
+  local CLUSTER="${1}"
+  echo "Configuring istio in cluster '${CLUSTER}' to use root CA on '${ROOTCA}'"
+
+  # create service account (delete it and its associated secret first, if they exist)
+  SERVICE_ACCOUNT=$(echo "istio-citadel-service-account-$CLUSTER" | tr '[:upper:]' '[:lower:]') #lower case to make it valid service account name
+  CERT_NAME="istio.${SERVICE_ACCOUNT}"
+
+  kubectl --context ${ROOTCA} -n ${NAMESPACE} delete serviceaccount ${SERVICE_ACCOUNT} --wait=true --ignore-not-found
+  kubectl --context ${ROOTCA} -n ${NAMESPACE} delete secret ${CERT_NAME} --wait=true --ignore-not-found
+
+  kubectl --context ${ROOTCA} -n ${NAMESPACE} create serviceaccount ${SERVICE_ACCOUNT}
   until kubectl --context ${ROOTCA} get -n ${NAMESPACE} secret ${CERT_NAME}
   do
     echo "waiting for the cert to be generated ..."
     sleep 1
   done
 
+  # create secret/cacerts (delete it first, if it exists)
+  kubectl --context ${CLUSTER} delete secret cacerts -n ${NAMESPACE} --ignore-not-found
+
+  DIR="/tmp/ca/${CLUSTER}"
+  mkdir -p ${DIR}; rm -rf ${DIR}/*
   kubectl --context ${ROOTCA} get -n ${NAMESPACE} secret $CERT_NAME -o jsonpath='{.data.root-cert\.pem}' | $B64_DECODE   > ${DIR}/root-cert.pem
   kubectl --context ${ROOTCA} get -n ${NAMESPACE} secret $CERT_NAME -o jsonpath='{.data.cert-chain\.pem}' | $B64_DECODE  > ${DIR}/cert-chain.pem
   kubectl --context ${ROOTCA} get -n ${NAMESPACE} secret $CERT_NAME -o jsonpath='{.data.key\.pem}' | $B64_DECODE   > ${DIR}/ca-key.pem
   cp ${DIR}/cert-chain.pem ${DIR}/ca-cert.pem
 
-  kubectl --context ${CLUSTER} create secret generic cacerts -n istio-system \
+  kubectl --context ${CLUSTER} create secret generic cacerts -n ${NAMESPACE} \
           --from-file=${DIR}/ca-cert.pem --from-file=${DIR}/ca-key.pem \
-          --from-file=${DIR}/root-cert.pem --from-file=${DIR}/cert-chain.pem || true
+          --from-file=${DIR}/root-cert.pem --from-file=${DIR}/cert-chain.pem
 
-  kubectl --context ${CLUSTER} delete  deployment  -n istio-system  istio-citadel --ignore-not-found
+  # recreate istio-citadel deployment to use upstream standalone citadel
+  kubectl --context ${CLUSTER} delete  deployment  -n ${NAMESPACE}  istio-citadel --ignore-not-found
+  standalone_svc="istio-citadel-new-svc-lb.yaml"
   if [[ -n $rootca_port ]]; then
-    sed -e "s/__ROOTCA_HOST__/${rootca_host}/g;s/__ROOTCA_PORT__/${rootca_port}/g" istio-citadel-new-svc-nolb.yaml | kubectl --context ${CLUSTER} apply -f -
-  else
-    sed -e "s/__ROOTCA_HOST__/${rootca_host}/g" istio-citadel-new-svc-lb.yaml | kubectl --context ${CLUSTER} apply -f -
+    standalone_svc=istio-citadel-new-svc-nolb.yaml
   fi
-  sed -e "s/__SERVICE_ACCOUNT__/${SERVICE_ACCOUNT}/g" istio-citadel.yaml | kubectl --context ${CLUSTER} apply -f -
-  kubectl --context ${CLUSTER} apply -f  istio-auto-injection.yaml || true
+  sed -e "s/__ROOTCA_HOST__/${rootca_host}/g;s/__ROOTCA_PORT__/${rootca_port}/g" ${standalone_svc} | tee ${DIR}/svc.yaml | kubectl --context ${CLUSTER} apply -f -
+  sed -e "s/__SERVICE_ACCOUNT__/${SERVICE_ACCOUNT}/g" istio-citadel.yaml | tee ${DIR}/istio-citadel.yaml | kubectl --context ${CLUSTER} apply -f -
+
+  # Do basic validatation of config -- check that istio-citadel starts
+  end=$((SECONDS+10))
+  until (( $SECONDS > $end )) || \
+        [[ 1 == $(kubectl --context ${CLUSTER} -n ${NAMESPACE} get deploy istio-citadel -o jsonpath='{.status.readyReplicas}') ]]
+  do
+    sleep 3
+  done
+  if [[ "1" != $(kubectl --context ${CLUSTER} -n ${NAMESPACE} get deploy istio-citadel -o jsonpath='{.status.readyReplicas}') ]]; then
+    echo "ERROR: istio-citadel not started correctly. Please check configuration."
+    exit 1
+  fi
+}
+
+############################################
+######## main
+############################################
+if [[ -z "${ROOTCA}" ]]; then
+  usage
+fi
+echo "Using root CA on cluster '$ROOTCA'"
+
+if [[ -n ${INSTALL_ROOTCA} ]]; then
+  install_root_ca 
+fi
+ 
+# Identify root CA ip (and port, if needed)
+rootca_host=`kubectl --context ${ROOTCA} get service standalone-citadel -n ${NAMESPACE} -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
+# handle case of no LB (ex ICP)
+if [[ -z $rootca_host ]]; then
+  rootca_host=`kubectl --context ${ROOTCA} get nodes | grep proxy | awk '{print $1}'`
+  rootca_port=`kubectl --context ${ROOTCA} get service standalone-citadel -n ${NAMESPACE} -o jsonpath='{.spec.ports[0].nodePort}'`
+fi
+
+for CLUSTER in  "${CLUSTERS[@]}"; do
+  configure_client_cluster ${CLUSTER}
 done
